0eef8229113b23e1827c1e54627498592a9ffad7
Idiot <idiotonastic@hotmail.com>
2021-03-22 12:31:57 +0000

    Add project files.

---
+++ Data/Scripts/TeaShield.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Text;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRage.Utils;
+using VRageMath;
+using VRage.Game.Components;
+using Sandbox.ModAPI;
+using Sandbox.Game.EntityComponents;
+using VRage.ModAPI;
+using TeaSheilds.Shields;
+using VRage.ObjectBuilders;
+
+namespace TeaSheilds.Data.Scripts
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_EntityBase), false, new string[] { "LargeGridSmallShield", "LargeGridLargeShield", "SmallGridLargeShield", "SmallGridSmallShield" })]
+
+    public class TeaShield : MyGameLogicComponent
+    {
+
+        bool isInit = false;
+        int delay = 3;
+        bool added = false;
+        IMySlimBlock sBlock;
+        IMyFunctionalBlock fBlock;
+        Sandbox.ModAPI.IMyTerminalBlock sbBlock;
+        float points = 0f;
+        float max = 0f;
+        int chargeBlink = 0;
+        bool blink = false;
+        int lx = 1;
+        MyResourceSinkComponent sink;
+        MyDefinitionId electricity = MyResourceDistributorComponent.ElectricityId;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            try
+            {
+                base.Init(objectBuilder);
+                fBlock = Entity as IMyFunctionalBlock;
+                sBlock = fBlock.SlimBlock;
+                sbBlock = Entity as Sandbox.ModAPI.IMyTerminalBlock;
+                NeedsUpdate = MyEntityUpdateEnum.EACH_10TH_FRAME;
+                isInit = true;
+            }
+            catch (Exception ex)
+            {
+                Echo("IAShields threw init exception", ex.ToString());
+            }
+        }
+
+        public override void Close()
+        {
+            TeaShieldBase.shieldList.Remove(sBlock);
+        }
+
+        public void appendCustomInfo(Sandbox.ModAPI.IMyTerminalBlock block, StringBuilder info)
+        {
+            info.Clear();
+            info.AppendLine("Max Strength: " + ((int)max).ToString() + " points");
+            info.AppendLine("Current Strength: " + ((int)points).ToString() + " points");
+        }
+
+        public float powerCalc()
+        {
+            try
+            {
+                var power = 0f;
+                if ((Entity as IMyFunctionalBlock).Enabled)
+                    power = (float)((1 - (points / max)) * max / 1000);
+                return power;
+            }
+            catch (Exception ex)
+            {
+                Echo("IAShields threw exception", ex.ToString());
+                return 0f;
+            }
+        }
+
+        public override void UpdateBeforeSimulation10()
+        {
+            if (!isInit)
+                return;
+
+            if (!added)
+                initMe();
+
+            shieldStuff();
+        }
+
+        public void initMe()
+        {
+            try
+            {
+                max = getMax();
+                if (fBlock.Name != "NaN" && !fBlock.Name.Contains("-"))
+                    float.TryParse(fBlock.Name, out points);
+                if (points > max)
+                    points = 0f;
+                if (!TeaShieldBase.shieldList.Contains(sBlock))
+                    TeaShieldBase.shieldList.Add(sBlock);
+                sink = fBlock.ResourceSink as MyResourceSinkComponent;
+                sink.SetMaxRequiredInputByType(electricity, max / 1000);
+                sink.SetRequiredInputFuncByType(electricity, powerCalc);
+                sink.Update();
+                fBlock.AppendingCustomInfo += appendCustomInfo;
+                added = true;
+                for (int i = 1; i < 9; i++)
+                    sbBlock.SetEmissiveParts("Emissive" + i, Color.Red, i == 6 ? 1f : 0f);
+            }
+            catch (Exception ex)
+            {
+                Echo("IAShields threw exception", ex.ToString());
+            }
+        }
+
+        public void shieldStuff()
+        {
+            try
+            {
+                points = TeaShieldBase.recharge(fBlock, max, fBlock.Enabled && sink.IsPowerAvailable(electricity, powerCalc()));
+                sink.Update();
+
+                if (!fBlock.Enabled || !fBlock.IsFunctional || !sink.IsPowerAvailable(electricity, powerCalc()))
+                    for (int i = 1; i < 9; i++)
+                        sbBlock.SetEmissiveParts("Emissive" + i, Color.Red, i == 6 ? 1f : 0f);
+                else
+                {
+                    sbBlock.SetEmissiveParts("Emissive7", Color.Yellow, 0.25f);
+                    sbBlock.SetEmissiveParts("Emissive8", Color.White, 1f);
+                    var perc = points / max;
+                    var blinkCalc = (int)(perc * 5) + 1;
+                    if (perc < 0.99f)
+                    {
+                        if (++chargeBlink > blinkCalc)
+                        {
+                            chargeBlink = 0;
+                            blink = !blink;
+                            for (int i = 1; i < 5; i++)
+                                sbBlock.SetEmissiveParts("Emissive" + i, colour(perc), i == lx ? 1f : 0f);
+                            lx = lx == 4 ? 1 : lx + 1;
+                        }
+                        sbBlock.SetEmissiveParts("Emissive5", colour(perc), blink ? (1.3f - perc) : 0.1f);
+                        sbBlock.SetEmissiveParts("Emissive6", perc < 0.9f ? Color.Cyan : Color.Green, 1f);
+                    }
+                    else
+                        for (int i = 1; i < 7; i++)
+                            sbBlock.SetEmissiveParts("Emissive" + i, Color.Green, 1f);
+                }
+
+                fBlock.RefreshCustomInfo();
+            }
+            catch (Exception ex)
+            {
+                Echo("IAShields threw exception", ex.ToString());
+            }
+        }
+
+        public Color colour(float charge)
+        {
+            return ColorExtensions.HSVtoColor(new Vector3(charge / 3, 1f, 1f));
+        }
+
+        public float getMax()
+        {
+            if (sBlock.BlockDefinition.ToString().Contains("LargeGridSmallShield"))
+                return 50000f;
+            if (sBlock.BlockDefinition.ToString().Contains("LargeGridLargeShield"))
+                return 400000f;
+            if (sBlock.BlockDefinition.ToString().Contains("SmallGridLargeShield"))
+                return 10000f;
+            return 1250f;
+        }
+
+        public static void Echo(string msg1, string msg2 = "")
+        {
+            //      MyAPIGateway.Utilities.ShowMessage(msg1, msg2);
+            MyLog.Default.WriteLineAndConsole(msg1 + ": " + msg2);
+        }
+
+    }
+}
---
+++ Data/Scripts/TeaShieldBase.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+        // get grid
+        // get grid centre
+        // draw sheild
+        // calc HP as size of sheild
+        // if entity enters sheild get entity damage and - form sheild hp
+        // if sheild hp after damage  < shield hp sheild = off do damage to blocks
+
+
+
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRage.Utils;
+using VRageMath;
+using VRage.Game.Components;
+using Sandbox.ModAPI;
+
+namespace TeaSheilds.Shields
+{
+
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+    public class TeaShieldBase : MySessionComponentBase
+    {
+
+        public static bool _isInit;
+        public static List<IMySlimBlock> shieldList;
+        public static Dictionary<IMySlimBlock, Vector3> vicDict;
+
+        public static int hue;
+
+        // Initialisation
+
+        public override void UpdateBeforeSimulation()
+        {
+            try
+            {
+                if (!_isInit)
+                    if (MyAPIGateway.Utilities.IsDedicated || MyAPIGateway.Session.Player != null)
+                    {
+                        shieldList = new List<IMySlimBlock>();
+                        vicDict = new Dictionary<IMySlimBlock, Vector3>();
+                        MyAPIGateway.Session.DamageSystem.RegisterBeforeDamageHandler(10, checkDamage);
+                        _isInit = true;
+                    }
+                    else
+                        return;
+                foreach (var pair in vicDict)
+                    pair.Key.CubeGrid.ColorBlocks(pair.Key.Min, pair.Key.Max, pair.Value);
+                vicDict.Clear();
+            }
+            catch (Exception ex)
+            {
+                Echo("IAShields threw init exception", ex.ToString());
+            }
+        }
+
+        // Mod actions
+
+        public static float recharge(IMyFunctionalBlock fBlock, float max, bool working)
+        {
+            // Charge reverts to 0 if shield damaged below functional, error with reading charge from Name, or some other random issue
+            float points = 0f;
+            if (!fBlock.IsFunctional || !float.TryParse(fBlock.Name, out points) || points < 0f || points.ToString() == "NaN")
+            {
+                fBlock.Name = 0f.ToString();
+                return 0f;
+            }
+
+            // If turned on, recharge at inverse rate. Fastest when discharged, slowest when nearly full. Energy usage is in direct proportion.
+            if (working)
+            {
+                points += max * 0.0025f * (1f - points / max);
+                if (points > max)
+                    points = max;
+            }
+
+            // Naming the block as its charge allows persistence through restarts
+            fBlock.Name = points.ToString();
+
+            return points;
+        }
+
+        public static void checkDamage(object victim, ref MyDamageInformation info)
+        {
+            try
+            {
+
+                // Shield only protects blocks, and not from grinding
+                if (!(victim is IMySlimBlock) || info.Amount == 0f || info.Type == MyDamageType.Grind)
+                    return;
+                var sBlock = victim as IMySlimBlock;
+
+                // Lowest impact method to get the active shields on target grid
+                var tempList = new List<IMySlimBlock>();
+                foreach (var shield in shieldList)
+                    if (shield.CubeGrid == sBlock.CubeGrid && shield.FatBlock.IsFunctional)
+                        tempList.Add(shield);
+
+                if (tempList.Count == 0)
+                    return;
+
+                // Get shield points and populate parallel dictionary (used to avoid foreach issues if a shield is simultaneously recharged)
+                var tempDict = new Dictionary<IMySlimBlock, float>();
+                float totalPoints = 0f;
+                foreach (var shield in tempList)
+                {
+                    float points = 0f;
+                    float.TryParse(shield.FatBlock.Name, out points);
+                    if (points.ToString() == "NaN" || points < 0f)
+                        points = 0f;
+                    tempDict.Add(shield, points);
+                    totalPoints += points;
+                }
+
+                if (info.Amount > totalPoints)
+                {
+                    // If shield strength is insufficient to stop damage, allow appropriate amount through
+                    foreach (var shield in tempList)
+                        tempDict[shield] = 0f;
+                    info.Amount -= totalPoints;
+                }
+                else
+                {
+                    // If shield strength is sufficient to stop damage, proportionally reduce shield(s) strength
+                    foreach (var shield in tempList)
+                    {
+                        tempDict[shield] -= (info.Amount * (tempDict[shield] / totalPoints));
+                        if (tempDict[shield] < 0f || tempDict[shield].ToString() == "NaN")
+                            tempDict[shield] = 0f;
+                    }
+                    info.Amount = 0f;
+
+                    // Show effect
+                    //          if (info.Amount > 1f)
+                    {
+                        if (!vicDict.ContainsKey(sBlock))
+                            vicDict.Add(sBlock, sBlock.GetColorMask());
+                        float hue = 0.3f - info.Amount / totalPoints / 3f;
+                        sBlock.CubeGrid.ColorBlocks(sBlock.Min, sBlock.Max, new Vector3(hue, 1f, 1f));
+                    }
+                }
+
+                // Update block names with current points
+                foreach (var pair in tempDict)
+                    pair.Key.FatBlock.Name = pair.Value.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Echo("IAShields exception", ex.ToString());
+            }
+        }
+
+        public static void Echo(string msg1, string msg2 = "")
+        {
+            //      MyAPIGateway.Utilities.ShowMessage(msg1, msg2);
+            MyLog.Default.WriteLineAndConsole(msg1 + ": " + msg2);
+        }
+    }
+}
